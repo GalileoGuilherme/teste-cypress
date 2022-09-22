65ed89ffd6d94cd03bd323c96fdd8e88cbf6cff4
Galileo Guilherme <galileoguilherme@outlook.com>
2022-09-21 22:06:47 -0300

    update

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+context('Funcionalidade Login', () => {
+
+    it('Deve fazer login com sucesso' , () =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('a > .hidden-xs').should('contain', 'Welcome')
+    })
+
+    it('Deve exibir um mensagem de erro ao inserir usuário inválido', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('luno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        
+        cy.get('.woocommerce-error > li').should('contain','desconhecido')
+    });
+
+    it('Deve exibir um mensagem de erro ao inserir senha inválida', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('este@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error > li').should('contain', 'incorreta')
+    });
+});
