0c7ddff60618cfb7c5968938a4400b1e61797838
Galileo Guilherme <galileoguilherme@outlook.com>
2022-09-23 22:31:08 -0300

    update_pre_cadastro

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -1,24 +1,28 @@
 /// <reference types="cypress" />
-let faker = require('faker');
+import { faker } from '@faker-js/faker';
+faker.locale = 'pt_BR';
+// var faker = require('faker');
 
-context('Funcionalidade Pré Cadastro', () => {
+describe('Funcionalidade Pré Cadastro', () => {
     
        beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
        });
        
        it('Deve completar o pre cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
-        cy.get('#reg_password').type('teste@teste.com')
-        cy.get(':nth-child(4) > .button').click
+              cy.get('#reg_email').type(faker.internet.email())
+              cy.get('#reg_password').type(faker.internet.password())
+              // cy.get(':nth-child(4) > .button').click
+              
+              cy.get(':nth-child(4) > .button').type('Cypress.io{enter}')
 
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click
+              cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click
 
-        cy.get('#account_first_name').type('Galileo')
-        cy.get('#account_last_name').type('Silva')
-        cy.get('#account_display_name')
-        cy.get('#account_email')
-        cy.get('.woocommerce-Button').click()
+              cy.get('#account_first_name').type(faker.name.firstName())
+              cy.get('#account_last_name').type(faker.name.lastName())
+              cy.get('#account_display_name')
+              cy.get('#account_email')
+              cy.get('.woocommerce-Button').click()
 
 
        });
